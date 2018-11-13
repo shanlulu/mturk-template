81de03bb73ad7ccd43d018dbbb4cd82f20e31ddd
Shan <slu5@wellesley.edu>
2018-11-13 00:13:09 -0500

    deploy simple server to heroku

--- server/server.py
+++ server/server.py
@@ -23,28 +23,28 @@ import json
 import uuid
 from urllib.parse import parse_qs
 
-from save_data import save_locally, save_s3 
+from save_data import save_locally, save_s3
 COUNTER = "server/counter.txt"
-INDEX = "index.html"
+# INDEX = "index.html"
 
 SAVE_LOCALLY=True
 
-def get_querystring(path): 
+def get_querystring(path):
     pairs = {}
     qstring = path.split('?')
-    if len(qstring) == 1: 
-        return {} 
-    else: 
-        qstring = qstring[-1]   
+    if len(qstring) == 1:
+        return {}
+    else:
+        qstring = qstring[-1]
     path = qstring.split('&')
-    for pair in path: 
+    for pair in path:
         key, val = pair.split('=')
         pairs[key] = val
     return pairs
 
-def strip_qstring(path): 
+def strip_qstring(path):
     p = path.split('?')
-    if len(p) == 1: 
+    if len(p) == 1:
         return path
     return "".join(p[:-1])
 
@@ -54,56 +54,57 @@ class S(SimpleHTTPRequestHandler):
         self.send_header('Content-type', 'text/html')
         self.end_headers()
 
-    def _copyfile(self, source, dest): 
+    def _copyfile(self, source, dest):
         shutil.copyfileobj(source, dest);
 
     def do_GET(self):
         print("GETTING")
         base_path = strip_qstring(self.path)
+        print(base_path)
         try:
-            if (base_path == '/counter'): 
+            if (base_path == '/counter'):
                 self._set_headers()
-                with open(COUNTER, 'r+') as infile: 
+                with open(COUNTER, 'r+') as infile:
                         counter = int(infile.read().strip())
                         infile.seek(0)
                         infile.write(str(counter + 1))
                 self.wfile.write(str(counter).encode())
                 return
-            elif (base_path == '/reset'): 
+            elif (base_path == '/reset'):
                 self._set_headers()
-                with open(COUNTER, 'w') as outfile: 
+                with open(COUNTER, 'w') as outfile:
                         outfile.write(str(0))
                 self.wfile.write(str(0).encode())
                 return
-            elif (base_path == '/view'): 
+            elif (base_path == '/view'):
                 self._set_headers()
-                with open(COUNTER, 'r') as infile: 
+                with open(COUNTER, 'r') as infile:
                     counter = int(infile.read().strip())
                     self.wfile.write(str(counter).encode())
                 return
-            else: 
+            else:
                 # override the default behavior of the simple handler
                 super(S, self).do_GET()
-        except Exception as e: 
+        except Exception as e:
             traceback.print_exc()
             self.send_error(500, "Internal server error: {}".format(e))
 
     def do_HEAD(self):
         self._set_headers()
-        
-    def do_POST(self):
-        # When it receives a POST request, it parses the data as json and creates a new file 
-        # to save the data. 
-        # the file name is fixed for rn. 
-        try: 
-            if (self.path == "/data"): 
+
+    def do_POST(self): #name=windard&year=21
+        # When it receives a POST request, it parses the data as json and creates a new file
+        # to save the data.
+        # the file name is fixed for rn.
+        try:
+            if (self.path == "/data"):
                 self._set_headers()
                 data = self.rfile.read(int(self.headers['Content-Length']))
                 data = data.decode("utf-8")
                 data = parse_qs(data)
                 print("DATA", data)
                 key = str(uuid.uuid4())
-                if SAVE_LOCALLY: 
+                if SAVE_LOCALLY:
                     save_locally(key, data)
                 else:
                     save_s3(key, data)
@@ -111,16 +112,16 @@ class S(SimpleHTTPRequestHandler):
                 response = json.dumps({'key': key})
                 self.send_response(200)
                 self.wfile.write(response.encode())
-            else: 
+            else:
                 self.send_error(400, "Page does not exist")
-        except Exception as e: 
+        except Exception as e:
             traceback.print_exc()
             self.send_error(500, "Internal server error: {}".format(e))
-        
-def run(server_class=HTTPServer, handler_class=S, port=8000):
-    # if we are on heroku, set the port according to heroku's instructions 
-    ON_HEROKU = os.environ.get('ON_HEROKU', False)    
-    if ON_HEROKU: 
+
+def run(server_class=HTTPServer, handler_class=S, port=8010):
+    # if we are on heroku, set the port according to heroku's instructions
+    ON_HEROKU = os.environ.get('ON_HEROKU', False)
+    if ON_HEROKU:
         port = int(os.environ.get('PORT'))
         print("Found port via Heroku: %d", port)
 
@@ -131,7 +132,6 @@ def run(server_class=HTTPServer, handler_class=S, port=8000):
 
 if __name__ == "__main__":
     from sys import argv
-
     if len(argv) == 2:
         run(port=int(argv[1]))
     else:
